318c3c61c8ec0cf1e21dd3001654ada41fb6dcf7
Natanael Lima <taelima1997@gmail.com>
2021-10-11 22:37:46 -0300

    update with emailsent usereducer

--- src/screens/Plan/index.tsx
+++ src/screens/Plan/index.tsx
@@ -1,32 +1,34 @@
-import React, { useReducer, useState } from 'react';
-import { View, Text, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import React, { useReducer } from "react";
+import { View, Text, TouchableWithoutFeedback, Keyboard } from "react-native";
 
-import { Input } from '../../components/Input';
-import { Header } from '../../components/Header';
-import { Option } from '../../components/Option';
-import { Button } from '../../components/Button';
-import { PlanInfo, PlanInfoProps } from '../../components/PlanInfo';
+import { Input } from "../../components/Input";
+import { Header } from "../../components/Header";
+import { Option } from "../../components/Option";
+import { Button } from "../../components/Button";
+import { PlanInfo } from "../../components/PlanInfo";
 
-import { styles } from './styles';
-import { ReducerPlan } from './reducer';
+import { styles } from "./styles";
+import { ReducerPlan } from "./reducer";
 
-const initialState = { plan: { name: "Basic", value: "5.25" } };
+const initialState = {
+  plan: { name: "Basic", value: "5.25" },
+  emailSent: false,
+};
 
 export function Plan() {
   const [state, dispatch] = useReducer(ReducerPlan, initialState);
-  const [emailSent, setEmailSent] = useState(false);
 
-  console.log(state)
+  console.log(state);
   function handleChangePlan(plan: string) {
-    if (plan === 'basic') {
-        dispatch({type: 'PLAN_BASIC'})
+    if (plan === "basic") {
+      dispatch({ type: "PLAN_BASIC" });
     } else {
-        dispatch({type: 'PLAN_PREMIUM'})
+      dispatch({ type: "PLAN_PREMIUM" });
     }
   }
 
   function handleSubscribe() {
-    setEmailSent(true);
+    dispatch({ type: "EMAIL_SENT_TRUE" });
   }
 
   return (
@@ -34,37 +36,30 @@ export function Plan() {
       <View style={styles.container}>
         <Header />
 
-        <PlanInfo
-          name={state.plan.name}
-          value={state.plan.value}
-        />
+        <PlanInfo name={state.plan.name} value={state.plan.value} />
 
         <View style={styles.options}>
           <Option
             title="Premium"
-            active={state.plan.name === 'Premium'}
-            onPress={() => handleChangePlan('premium')}
+            active={state.plan.name === "Premium"}
+            onPress={() => handleChangePlan("premium")}
             testID="option-premium"
           />
           <Option
             title="Basic"
-            active={state.plan.name === 'Basic'}
-            onPress={() => handleChangePlan('basic')}
+            active={state.plan.name === "Basic"}
+            onPress={() => handleChangePlan("basic")}
           />
         </View>
 
-        <Input
-          placeholder="your email"
-          testID="input-email"
-        />
+        <Input placeholder="your email" testID="input-email" />
 
-        {
-          emailSent &&
+        {state.emailSent && (
           <Text style={styles.confirmation} testID="confirmation-message">
-            We send you a  {'\n'}
+            We send you a {"\n"}
             confirmation email.
           </Text>
-        }
+        )}
 
         <Button
           title="Subscribe"
@@ -73,8 +68,8 @@ export function Plan() {
         />
 
         <Text style={styles.details} testID="plan-note">
-          If the price changes, we'll notify you beforehand.
-          You cam check your renewal date or cancel anytime via your Account page.
+          If the price changes, we'll notify you beforehand. You cam check your
+          renewal date or cancel anytime via your Account page.
         </Text>
       </View>
     </TouchableWithoutFeedback>
