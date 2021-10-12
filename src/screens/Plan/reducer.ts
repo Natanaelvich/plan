210f8cca031d54d23cdf7a573722fcab6b237bc6
Natanael Lima <taelima1997@gmail.com>
2021-10-11 22:37:59 -0300

    add type and cases email

--- src/screens/Plan/reducer.ts
+++ src/screens/Plan/reducer.ts
@@ -1,15 +1,31 @@
+import { PlanInfoProps } from "../../components/PlanInfo";
 
+type StateProps = {
+  plan: PlanInfoProps;
+  emailSent: boolean;
+};
 
-export function ReducerPlan(_ : any, action: any) {
+export function ReducerPlan(state: StateProps, action: any) {
   switch (action.type) {
     case "PLAN_BASIC":
-      return { plan: { name: "Basic", value: "5.25" } };
+      return { ...state, plan: { name: "Basic", value: "5.25" } };
     case "PLAN_PREMIUM":
       return {
+        ...state,
         plan: {
           name: "Premium",
           value: "6.99",
         },
+      };
+    case "EMAIL_SENT_TRUE":
+      return {
+        ...state,
+        emailSent: true,
+      };
+    case "EMAIL_SENT_FALSE":
+      return {
+        ...state,
+        emailSent: false,
       };
     default:
       throw new Error();
