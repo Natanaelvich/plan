b27676b8e55bb01c778445d9bc0bd48d001ce54b
Natanael Lima <taelima1997@gmail.com>
2021-10-11 23:09:55 -0300

    add implementation with forwardRef and useImperativeHandle

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,13 +1,26 @@
-import React from 'react';
-import { TextInput, TextInputProps } from 'react-native';
+import React, {
+  forwardRef,
+  useImperativeHandle,
+  useRef,
+} from "react";
+import { TextInput, TextInputProps } from "react-native";
 
-import { styles } from './styles';
+import { styles } from "./styles";
 
-export function Input({ ...rest }: TextInputProps) {
-  return (
-    <TextInput
-      style={styles.input}
-      {...rest}
-    />
-  );
-}
+type InputHandle = {
+  focus: () => void;
+};
+
+const Input = forwardRef<InputHandle, TextInputProps>(({ ...rest }, ref) => {
+  const inputRef = useRef<TextInput>();
+
+  useImperativeHandle(ref, () => ({
+    focus: () => {
+      inputRef.current?.focus();
+    },
+  }));
+
+  return <TextInput ref={inputRef as any} style={styles.input} {...rest} />;
+});
+
+export default Input;
