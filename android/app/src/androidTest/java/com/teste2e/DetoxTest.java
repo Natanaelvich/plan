9d0425c040ba5cb0c80dcfad57c6082532adcbc5
Natanael Lima <taelima1997@gmail.com>
2021-10-12 23:35:35 -0300

    fix config detox android

--- android/app/src/androidTest/java/com/teste2e/DetoxTest.java
+++ android/app/src/androidTest/java/com/teste2e/DetoxTest.java
@@ -22,7 +22,7 @@ public class DetoxTest {
         DetoxConfig detoxConfig = new DetoxConfig();
         detoxConfig.idlePolicyConfig.masterTimeoutSec = 90;
         detoxConfig.idlePolicyConfig.idleResourceTimeoutSec = 60;
-        detoxConfig.rnContextLoadTimeoutSec = (com.package.BuildConfig.DEBUG ? 180 : 60);
+        detoxConfig.rnContextLoadTimeoutSec = (com.teste2e.BuildConfig.DEBUG ? 180 : 60);
 
         Detox.runTests(mActivityRule, detoxConfig);
     }
